704ec9b45754455dd6413595258fe0daf26800b8
smilekeepers-foundation <mayureshnaik662@gmail.com>
2025-11-14 22:56:23 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  reactStrictMode: false
+  reactStrictMode: false,
+
+  // REQUIRED for GitHub Pages static export
+  images: {
+    unoptimized: true,
+  },
+
+  // REQUIRED for next export (static HTML)
+  output: "export",
 };
 
 module.exports = nextConfig;
